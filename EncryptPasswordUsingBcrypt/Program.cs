cbf19dca17ffd89e1f02abcff33fe29f0f91ba87
Rakeshkumar Desai <innovativetechiesltd@gmail.com>
2020-04-01 19:18:37 +0530

    Added RIP pattern and Bcrypt library usage

---
+++ EncryptPasswordUsingBcrypt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BCrypt.Net;
+namespace EncryptPasswordUsingBcrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var plainPassword = Console.ReadLine();
+
+            var EncryptedPassword = BCrypt.Net.BCrypt.HashPassword(plainPassword);
+
+            Console.WriteLine("Encrypted password is : {0}",EncryptedPassword);
+
+            var isValidPassword = BCrypt.Net.BCrypt.Verify("invalidPassword", EncryptedPassword);
+
+            Console.WriteLine(isValidPassword);
+        }
+    }
+}
---
+++ ReplaceIfWithPloyMorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReplaceIfWithPloyMorphism
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            vehicles.Add(new TwoWheeler());
+            vehicles.Add(new FourWheeler());
+
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(vehicles[i].GetType().Name);
+        }
+    }
+
+    public class Vehicle
+    {
+
+    }
+
+    public class TwoWheeler:Vehicle
+    {
+
+    }
+    public class FourWheeler : Vehicle
+    {
+
+    }
+}
